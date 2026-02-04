b08a75928db2de07b8604f9173eaf11e11cd4d14
Akihisa Watanabe <watanabe.a@nttdata-gsl.co.jp>
2026-02-04 09:39:28 +0000

    commit

---
+++ helloworldui5/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("sap.btp.helloworldui5.controller.View1", {
+        onInit() {
+        }
+    });
+});
